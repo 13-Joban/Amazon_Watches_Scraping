ec525c4b3550e709064bb81a2d33a62d6a3fa608
Jobanpreet <jobanjatt113@gmail.com>
2024-01-16 15:28:29 +0530

    resolved multiple error

--- amazonscraping/amazonscraping/spiders/amazon_spider.py
+++ amazonscraping/amazonscraping/spiders/amazon_spider.py
@@ -10,24 +10,29 @@ class AmazonSpiderSpider(scrapy.Spider):
     def parse(self, response):
         items = AmazonscrapingItem()
         # print(items)
-        product_brand = response.xpath("//span[@class='a-size-base-plus a-color-base']/text()").extract()
-        product_desc = response.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal']/text()").extract()
-        product_price = response.xpath("//span[@class='a-price']/span[@class='a-offscreen']/text()").extract()
-        product_url = response.xpath("//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']/@href").extract()
+        total_products = response.xpath("//div[@class='sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20']")
 
+        for product in total_products:
 
-        items['product_brand'] = product_brand
-        items['product_price'] = product_price
-        items['product_desc'] = product_desc
-        items['product_url'] =  product_url
+            product_brand = product.xpath(".//span[@class='a-size-base-plus a-color-base']/text()").extract()[0]
+            product_desc = product.xpath(".//span[@class='a-size-base-plus a-color-base a-text-normal']/text()").extract()[0]
+            product_price_list = product.xpath(".//span[@class='a-price']/span[@class='a-offscreen']/text()").extract()
+            product_price = product_price_list[0] if product_price_list else ''
 
-        yield items
+            # product_price = product.xpath(".//span[@class='a-price']/span[@class='a-offscreen']/text()").extract()[0]
+            product_url = product.xpath(".//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']/@href").extract()[0]
 
-        next_page = 'https://www.amazon.in/s?k=watch+for+men&i=watches&rh=n%3A2563504031%2Cp_72%3A1318476031%2Cp_36%3A3439818031%2Cp_n_feature_fourteen_browse-bin%3A11142592031&s=price-asc-rank&dc&page='  + str(self.page_number)+ '&crid=25PG7RN3QLXY7&qid=1705343446&rnid=11142591031&sprefix=watch+for+men%2Caps%2C215&ref=sr_pg_2'
+            items['product_brand'] = product_brand
+            items['product_price'] = product_price
+            items['product_desc'] = product_desc
+            items['product_url'] = 'https://www.amazon.in' + product_url
 
-        if self.page_number <= 19:
-            self.page_number += 1
-            yield response.follow(next_page, callback=self.parse)
+            yield items
 
+        next_page = 'https://www.amazon.in/s?k=watch+for+men&i=watches&rh=n%3A2563504031%2Cp_72%3A1318476031%2Cp_36%3A3439818031%2Cp_n_feature_fourteen_browse-bin%3A11142592031&s=price-asc-rank&dc&page=' + str(AmazonSpiderSpider.page_number) + '&crid=25PG7RN3QLXY7&qid=1705343446&rnid=11142591031&sprefix=watch+for+men%2Caps%2C215&ref=sr_pg_2'
 
+        print(next_page)
 
+        if AmazonSpiderSpider.page_number <= 19:
+            AmazonSpiderSpider.page_number += 1
+            yield scrapy.Request(url=next_page, callback=self.parse)
